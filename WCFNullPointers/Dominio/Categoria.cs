1d22d0a1b1a2e5d3da3f5742e06710a9b47682b4
carloxvg <carloxvg@outlook.com>
2017-04-13 19:10:24 -0500

    Categoría Crear

---
+++ WCFNullPointers/Dominio/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCFNullPointers.Dominio
+{
+    [DataContract]
+    public class Categoria
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public int Estado { get; set; }
+    }
+}

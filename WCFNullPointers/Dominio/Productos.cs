f0674e052951bd4fce5a41de39af9700e9fd6f6a
Chiaris <chiarachirinos@gmail.com>
2017-04-13 21:34:22 -0500

    Creacion de método de la Tabla Productos

---
+++ WCFNullPointers/Dominio/Productos.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCFNullPointers.Dominio
+{
+    [DataContract]
+    public class Productos
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Nombre { get; set; }
+        [DataMember]
+        public string Descripcion { get; set; }
+        [DataMember]
+        public string Precio { get; set; }
+        [DataMember]
+        public string Descuento { get; set; }
+        [DataMember]
+        public string Presentacion { get; set; }
+        [DataMember]
+        public string CategoriaId { get; set; }
+        [DataMember]
+        public string Estado { get; set; }
+       
+    }
+}
